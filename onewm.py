c84daf8988b6b0a3cff2f8f49c6c7cf8ba71744a
Ethan Hill <ethan.hill1999@outlook.com>
2024-06-14 08:09:57 -0600

    init commit

---
+++ onewm.py
@@ -0,0 +1,40 @@
+from Xlib.display import Display
+from Xlib import X, XK
+
+# Set up variables
+dpy = Display()
+root = dpy.screen().root
+super_keycode = dpy.keysym_to_keycode(XK.string_to_keysym("Super_L"))
+window_ids = []
+current_index = 0
+root.grab_key(super_keycode, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync)
+first = True
+
+def fullscreen_window(window_ids, id):
+    window_id = window_ids[id]
+    for other_id in window_ids:
+        if other_id != window_id:
+            other_window = dpy.create_resource_object('window', other_id)
+            other_window.unmap()
+    window = dpy.create_resource_object('window', window_id)
+    window.configure(width=root.get_geometry().width, height=root.get_geometry().height)
+    window.map()
+
+while True:
+    ev = dpy.next_event()
+
+    # Swap between windows
+    if ev.type == X.KeyPress and ev.detail == super_keycode:
+        if window_ids:
+            current_window = dpy.create_resource_object('window', window_ids[current_index])
+            current_index = (current_index + 1) % len(window_ids)
+            fullscreen_window(window_ids, current_index)
+
+    # Bring window to front if just opened
+    elif ev.type == X.CreateNotify or first:
+        first = False
+        window_ids = []
+        for window in root.query_tree().children:
+            window_ids.append(window.id)
+        current_index = window_ids[-1]
+        fullscreen_window(window_ids, current_index)
