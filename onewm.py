90d70a00d549e2d5579d5106a7110adb16190788
Ethan Hill <ethan.hill1999@outlook.com>
2024-06-16 02:53:09 -0500

    Update onewm.py

--- onewm.py
+++ onewm.py
@@ -8,27 +8,26 @@ super_keycode = dpy.keysym_to_keycode(XK.string_to_keysym("Super_L"))
 window_ids = []
 current_index = 0
 root.grab_key(super_keycode, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync)
-first = True
+
+def is_toplevel_window(window):
+    attrs = window.get_attributes()
+    return attrs.map_state == X.IsViewable
 
 def fullscreen_window(id):
     global window_ids
-    print("#" + str(id))
-    print(window_ids)
     window_id = window_ids[id]
-    for other_id in window_ids:
-        if other_id != window_id:
-            other_window = dpy.create_resource_object('window', other_id)
-            other_window.unmap()
     window = dpy.create_resource_object('window', window_id)
-    window.configure(width=root.get_geometry().width, height=root.get_geometry().height)
+    window.configure(stack_mode=X.Above, width=root.get_geometry().width, height=root.get_geometry().height) 
+    window.set_input_focus(X.RevertToParent, X.CurrentTime)
     window.map()
+    return None
 
 def update_windows():
     global window_ids
     window_ids = []
     for window in root.query_tree().children:
-        window_ids.append(window.id)
-    print(window_ids)
+        if is_toplevel_window(window):
+            window_ids.append(window.id)
 
 update_windows()
 while True:
@@ -36,13 +35,10 @@ while True:
 
     # Swap between windows
     if ev.type == X.KeyPress and ev.detail == super_keycode:
-        if window_ids:
-            current_window = dpy.create_resource_object('window', window_ids[current_index])
-            current_index = (current_index + 1) % len(window_ids)
-            fullscreen_window(current_index)
+        current_index = (current_index + 1) % len(window_ids)
+        title = fullscreen_window(current_index)
 
     # Bring window to front if just opened
-    elif ev.type == X.CreateNotify or first:
-        first = False
+    elif ev.type == X.CreateNotify:
         update_windows()
         fullscreen_window(-1)
