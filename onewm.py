a0c12690c236cc6892143f60647e6a6ee0498016
Ethan Hill <ethan.hill1999@outlook.com>
2024-06-15 19:02:34 -0600

    somewhat functional status

--- onewm.py
+++ onewm.py
@@ -10,7 +10,10 @@ current_index = 0
 root.grab_key(super_keycode, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync)
 first = True
 
-def fullscreen_window(window_ids, id):
+def fullscreen_window(id):
+    global window_ids
+    print("#" + str(id))
+    print(window_ids)
     window_id = window_ids[id]
     for other_id in window_ids:
         if other_id != window_id:
@@ -20,6 +23,14 @@ def fullscreen_window(window_ids, id):
     window.configure(width=root.get_geometry().width, height=root.get_geometry().height)
     window.map()
 
+def update_windows():
+    global window_ids
+    window_ids = []
+    for window in root.query_tree().children:
+        window_ids.append(window.id)
+    print(window_ids)
+
+update_windows()
 while True:
     ev = dpy.next_event()
 
@@ -28,13 +39,10 @@ while True:
         if window_ids:
             current_window = dpy.create_resource_object('window', window_ids[current_index])
             current_index = (current_index + 1) % len(window_ids)
-            fullscreen_window(window_ids, current_index)
+            fullscreen_window(current_index)
 
     # Bring window to front if just opened
     elif ev.type == X.CreateNotify or first:
         first = False
-        window_ids = []
-        for window in root.query_tree().children:
-            window_ids.append(window.id)
-        current_index = window_ids[-1]
-        fullscreen_window(window_ids, current_index)
+        update_windows()
+        fullscreen_window(-1)
